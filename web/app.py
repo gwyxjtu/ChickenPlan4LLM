e23b76ee250ee84033b1c75e88d66dee782e2dfb
KezhiAdore <1538001851@qq.com>
2024-10-26 14:28:04 +0800

    优化界面，添加code和result页面

--- web/app.py
+++ web/app.py
@@ -5,6 +5,7 @@ from streamlit_ace import st_ace
 from constant import (DEFAULT_DESCRIPTION, 
                       DEFAULT_JSON_DESCRIPTION,
                       DEFAULT_PARAMS,
+                      DEFAULT_CODE,
                       TEXT_AREA_HEIGHT,
                       CODE_EDITOR_HEIGHT
                           )
@@ -29,6 +30,9 @@ if "json_description" not in st.session_state:
 if "parameters" not in st.session_state:
     st.session_state["parameters"] = DEFAULT_PARAMS
 
+if "code" not in st.session_state:
+    st.session_state["code"] = DEFAULT_CODE
+
 
 # 页面内容
 def page_language2json():
@@ -38,6 +42,7 @@ def page_language2json():
     
     def generate_json():
         description = st.session_state.description
+        
         st.session_state.json_description = "{\n\"test\": 0\n}"
 
     def clear_json():
@@ -50,11 +55,11 @@ def page_language2json():
         st.session_state["description"] = description
         c1, c2, c3 = st.columns(3)
         with c1:
-            st.button("保存描述", on_click=save_description)
+            st.button("保存描述", on_click=save_description, use_container_width=True)
         with c2:
-            st.button("生成Json", on_click=generate_json)
+            st.button("生成Json", on_click=generate_json, use_container_width=True)
         with c3:
-            st.button("清空信息", on_click=clear_json)
+            st.button("清空信息", on_click=clear_json, use_container_width=True)
     with col2:
         st.text("JSON描述")
         json_description=st_ace(st.session_state.json_description, language="json", height=CODE_EDITOR_HEIGHT)
@@ -65,30 +70,45 @@ def page_json2param():
     def generate_params():
         description = st.session_state.description
         json_description = st.session_state.json_description
+        
         st.session_state.parameters = "{\n\"params\": 0\n}"
     
     col1, col2 = st.columns(2)
     with col1:
         st.text("JSON描述")
         st_ace(st.session_state.json_description, language="json", height=CODE_EDITOR_HEIGHT, readonly=True)
-        st.button("生成参数", on_click=generate_params)
+        st.button("生成参数", on_click=generate_params, use_container_width=True)
     with col2:
         st.text("JSON参数")
         params = st_ace(st.session_state.parameters, language="json", height=CODE_EDITOR_HEIGHT)
         st.session_state["parameters"] = params
-        st.button("求解")
+        st.button("求解", use_container_width=True)
 
 def page_param2code():
-    pass
+    code = st_ace(st.session_state.code, language="python", height=CODE_EDITOR_HEIGHT)
+    st.session_state["code"] = code
+    st.button("运行", 
+              use_container_width=True,
+              type="primary",
+              )
+
+def page_result():
+    st.text("运行结果")
+    st.dataframe({"a": [1, 2, 3], "b": [4, 5, 6]})
+    st.line_chart({"a": [1, 2, 3, 4], "b": [4,5,6,7]})
+    st.button("保存结果", use_container_width=True)
 
 # 页面标题
 st.title("Energy LLM")
 
 with st.sidebar:
-    selected_tab = option_menu("Workflow", ["Description", "Parameters", 
-                                            "Coding", "Data Processing", 
-                                            "Testing"], 
-        icons=['house']*5, menu_icon="cast", default_index=0)
+    selected_tab = option_menu(
+        menu_title="Workflow", 
+        menu_icon="robot", 
+        options=["Description", "Parameters", "Coding", "Results",], 
+        icons=['chat','sliders', 'braces','file-earmark-bar-graph'], # https://icons.getbootstrap.com/
+        default_index=0
+        )
 
 
 # 主内容区域
@@ -103,10 +123,7 @@ if selected_tab:
         page_json2param()
 
     elif selected_tab == "Coding":
-        st.write("Coding section is under construction.")
+        page_param2code()
 
-    elif selected_tab == "Data Processing":
-        st.write("Data Processing section is under construction.")
-
-    elif selected_tab == "Testing":
-        st.write("Testing section is under construction.")
+    elif selected_tab == "Results":
+        page_result()
